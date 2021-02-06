98c5dc7d8937913796761135529818dab463b17a
Toranova <chia_jason96@live.com>
2021-02-06 12:21:21 +0800

    debug p1

--- pages/focus.js
+++ pages/focus.js
@@ -151,6 +151,7 @@ class Focus extends Component {
 
 		this.socket.on('focus/alert/new', (res) => {
 			//start a timer, then emit an init later
+			console.log(res);
 			if(res.alert_model.id === 3){
 				// nvai
 				setTimeout( function(socket){
